e2131c0904c6a51f7c4f474fcb33eaac137863d7
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-06 00:17:54 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Rickandmortyapi from "./components/rickandmortyapi";
 
 const App = () => {
     return (
         <div>
-            
+            <Rickandmortyapi id={1} name={'Rick Sanchez'} status={'Alive'} species={'Human'} gender={'Male'}
+                             image={'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}/>
+            <Rickandmortyapi id={2} name={'Morty Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                             img={'https://rickandmortyapi.com/api/character/avatar/2.jpeg'}/>
+            <Rickandmortyapi id={3} name={'Summer Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                             img={'https://rickandmortyapi.com/api/character/avatar/3.jpeg'}/>
+            <Rickandmortyapi id={4} name={'Beth Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                             img={'https://rickandmortyapi.com/api/character/avatar/4.jpeg'}/>
+            <Rickandmortyapi id={5} name={'Jerry Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                             img={'https://rickandmortyapi.com/api/character/avatar/5.jpeg'}/>
+            <Rickandmortyapi id={6} name={'Abadango Cluster Princess'} status={'Alive'} species={'Alien'} gender={'Female'}
+                             image={'https://rickandmortyapi.com/api/character/avatar/6.jpeg'}/>
         </div>
     );
 };
--- src/components/rickandmortyapi.js
+++ src/components/rickandmortyapi.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Rickandmortyapi = () => {
+const Rickandmortyapi = (props) => {
+    let {id,name,status,species,gender,image} = props;
     return (
         <div>
-            
+            <h3>{id} {name}</h3>
+            <p>{status} {species} </p>
+            <p>{gender}</p>
+            <img src={image} alt={''}/>
         </div>
     );
 };
 
 export default Rickandmortyapi;
+// id,name,status,species,gender,image
