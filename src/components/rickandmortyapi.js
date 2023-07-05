caf9a2bd7e8c7ed8df14b408ee67e6fbd4f28c62
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-06 00:20:49 +0300

    com

--- src/components/rickandmortyapi.js
+++ src/components/rickandmortyapi.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './rickandStyle.css'
 
 const Rickandmortyapi = (props) => {
     let {id,name,status,species,gender,image} = props;
     return (
-        <div>
+        <div className={'main'}>
             <h3>{id} {name}</h3>
             <p>{status} {species} </p>
             <p>{gender}</p>
