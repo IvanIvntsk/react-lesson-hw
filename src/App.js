c8338bd834f6edbcc05e111963e926628b1bbbe2
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-07 19:16:46 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import  {posts} from './assets/mockData/posts'
-import PostComponent from  './Post/Post'
-import Post from "./Post/Post";
 
 const App = () => {
-    console.log(posts)
     return (
-
-          <>
-
-              {posts.map((post,id) =>
-                  (<PostComponent key={id} post={post}/>))}
-
-
-
-          </>
+        <div>
+            
+        </div>
     );
 };
 
