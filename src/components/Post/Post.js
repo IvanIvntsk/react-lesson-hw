e0323953812297465fc9783d05e2ae379bedcc29
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-08 15:58:00 +0300

    comi

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import PostDetail from "../postDetail/postDetail";
+import './post.css'
 
 const Post = ({post}) => {
     const {id, title} = post
     const [show, setShow] = useState(false)
     return (
-        <div>
+        <div className={'post'}>
             <h4>id: {id}</h4>
             <h3>{title}</h3>
-            <button onClick={()=>setShow(!show)}>{show?'Hide':'Show'}</button>
+            <button onClick={()=>setShow(!show)}>{show?'Hide':'Show Details'}</button>
             {
                 show&& <PostDetail post={post}/>
             }
--- src/components/postDetail/postDetail.js
+++ src/components/postDetail/postDetail.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './postDetail.css'
 
 const PostDetail = ({post}) => {
     const {id, userId, title, body} = post
     return (
-        <div>
-            <div>{id}</div>
-            <div>{userId}</div>
-            <div>{title}</div>
+        <div className={'detail'}>
+            <div><i>id:</i> {id}</div>
+            <div>User: {userId}</div>
+            <div>Title: {title}</div>
             <div>{body}</div>
         </div>
     );
