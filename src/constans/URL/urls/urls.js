3ee91699834d7b34a8925b532bfba9e7a5774f93
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-24 20:24:25 +0300

    start

---
+++ src/constans/URL/urls/urls.js
@@ -0,0 +1,22 @@
+export const baseURL = 'https://jsonplaceholder.typicode.com'
+
+const todos = '/todos'
+const albums = '/albums'
+const comment = '/comments'
+const posts = '/posts'
+
+export const urls = {
+    todos:{
+        base:todos,
+    },
+    albums:{
+        base:albums,
+    },
+    comment:{
+        base:comment,
+    },
+    posts:{
+      base:posts,
+      byId:(id)=>`${posts}/${id}`
+    }
+}
