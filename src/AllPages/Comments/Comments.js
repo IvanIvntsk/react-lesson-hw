e91693237edbe90165660f58190ac95ee670a522
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-25 23:07:05 +0300

    start 50%

--- src/AllPages/Comments/Comments.js
+++ src/AllPages/Comments/Comments.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import comments from "../../components/Comments/comments";
 import {Outlet} from "react-router-dom";
+import comments from "../../components/Comments/comments";
 
 
 
---
+++ src/components/Comments/comments.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const Comments = () => {
+   const[comments, setComments] = useState([])
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Comments;
