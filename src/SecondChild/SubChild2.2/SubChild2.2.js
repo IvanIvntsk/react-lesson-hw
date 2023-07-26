826694858ec6a84874f210fb142a003e7d85491c
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-07-26 22:00:35 +0300

    done

--- src/SecondChild/SubChild2.2/SubChild2.2.js
+++ src/SecondChild/SubChild2.2/SubChild2.2.js
@@ -3,6 +3,7 @@ import {Context} from "../../App";
 
 const SubChild22 = () => {
     const {setData} = useContext(Context)
+
     return (
         <div>
             <button onClick={()=>setData(prev=>[...prev, 'World'])}>AddInfo</button>
